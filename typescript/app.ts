cf5b7c371e0869884b200f6d0735bd56cbde1a40
Danniel Sousa <danniel.sousa@gmail.com>
2020-04-03 13:04:30 -0300

    Inferencia de tipos

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,5 +1,9 @@
-let ola = (nome: string, sobrenome: string) => {
-    console.log(`Olá ${nome} ${sobrenome}`);
-}
+let mensagem: string = 'Seja Bem vindo';
+let temporadasFreinds: number = 10;
+let esdudandoAngular: boolean = true;
 
-ola('danniel', 'sousa');
+let listaDeFrutas: Array<string> = ['Banana', 'Mação', 'Abacaxi'];
+let listaDeFrutas2: string[] = ['Banana', 'Mação', 'Abacaxi'];
+
+let notasDaProva: Array<number> = [10, 11.5, 9];
+let notasDaProva2: number[] = [10, 11.5, 9];
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,5 +1,8 @@
 "use strict";
-var ola = function (nome, sobrenome) {
-    console.log("Ol\u00E1 " + nome + " " + sobrenome);
-};
-ola('danniel', 'sousa');
+var mensagem = 'Seja Bem vindo';
+var temporadasFreinds = 10;
+var esdudandoAngular = true;
+var listaDeFrutas = ['Banana', 'Mação', 'Abacaxi'];
+var listaDeFrutas2 = ['Banana', 'Mação', 'Abacaxi'];
+var notasDaProva = [10, 11.5, 9];
+var notasDaProva2 = [10, 11.5, 9];
