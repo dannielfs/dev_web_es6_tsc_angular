e3f49a1212f8f708a57d39b36c164b47a21fa9a1
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-07 00:14:27 -0300

    alteraçoes para trazer a pesquisa com objeto nulo

--- projetos/app2/src/app/topo/topo.component.ts
+++ projetos/app2/src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../shared/services/ofertas.services';
 import { Oferta } from '../shared/model/oferta.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { switchMap, map, debounceTime,  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -10,21 +11,28 @@ import { Observable } from 'rxjs';
 })
 export class TopoComponent implements OnInit {
 
-  public ofertas: Observable<Oferta[]>
+  public ofertas: Observable<Oferta[]>;
+  private subjectPesquisa: Subject<string> = new Subject<string>();
+
   constructor(
     private ofertaService: OfertasService
   ) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      switchMap((termo: string) => {
+        if (termo.trim() === '') {
+          return of<Oferta[]>([]);
+        }
+        return this.ofertaService.pesquisaOfertas(termo);
+      })
+    )
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas));
   }
 
   public pesquisa(termoDaPesquisa: string): void {
-    this.ofertas = this.ofertaService.pesquisaOfertas(termoDaPesquisa);
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => {
-        console.log(ofertas);
-
-      }
-    )
+    this.subjectPesquisa.next(termoDaPesquisa)
   }
 }
