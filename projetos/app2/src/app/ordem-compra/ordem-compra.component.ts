4d6c6908e573e0ed1bafbde2f60f7d78659bf7c0
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-11 13:08:07 -0300

    Ajuste na forma de validaçao dos campos

--- projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -8,29 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class OrdemCompraComponent implements OnInit {
 
   public endereco: string = '';
-  public numero: string;
+  public numero: string = '';
   public complemento: string = '';
   public formaPagamento: string = '';
+
+  //controles de validacao dos campos
+
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
+
+  //estados primitivos
+
+  public enderecoEstadoPrimitivo: boolean = true;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementeEstadoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.formaPagamento);
+
   }
 
   atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
+    this.enderecoEstadoPrimitivo = false;
+    this.enderecoValido = this.endereco.length > 5 ? true : false;
   }
 
   atualizaNumero(numero: string): void {
     this.numero = numero;
+    this.numeroEstadoPrimitivo = false;
+    this.numeroValido = this.numero.length > 0 ? true : false;
   }
 
   atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
+    this.complementeEstadoPrimitivo = false;
+    this.complementoValido = this.complemento.length > 1 ? true : false;
   }
 
   atualizaFormaPagamento(pagamento: string): void {
     this.formaPagamento = pagamento;
+    this.formaPagamentoEstadoPrimitivo = false;
+    this.formaPagamentoValido = this.formaPagamento.length > 0 ? true : false;
   }
 
 }
